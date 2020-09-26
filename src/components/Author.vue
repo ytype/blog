92fa9eb0ba35260d4c1b38c50489bc5addb778d1
ytype <ytype33@gmail.com>
2020-09-26 23:47:16 +0900

    feat: add footer

--- src/components/Author.vue
+++ src/components/Author.vue
@@ -1,34 +1,48 @@
 <template>
-  <div class="author content has-text-centered">
+  <div class="main">
+    <hr
+      class="top"
+      style="margin-left: 15%;"
+    >
     <g-image
       alt="Author image"
-      class="image"
-      src="~/assets/images/author.jpg"
+      class="author-image"
+      src="~/assets/images/logo-square.png"
     />
-
-    <h1 v-if="showTitle">
-      {{ $static.metadata.siteName }}
-    </h1>
-
-    <p>A baseline Gridsome starter to get you going with Bulma.</p>
+    <hr
+      class="top"
+    >
+    <div class="content has-text-centered">
+      <p>A baseline Gridsome starter to get you going with Bulma.</p>
+    </div>
+    <hr class="bottom">
   </div>
 </template>
 
-<static-query>
-query {
-  metadata {
-    siteName
-  }
+<style lang="scss" scoped>
+.top {
+  display: inline-block;
+  width: calc((70% - 100px)/2);
+  vertical-align: middle;
+  border: 1px solid #202020;
 }
-</static-query>
-
-<script>
-export default {
-  props: {
-    showTitle: {
-      type: Boolean,
-      default: true
-    }
-  }
+.bottom {
+  margin-left: 15%;
+  display: inline-block;
+  width: 70%;
+  vertical-align: middle;
+  border: 1px solid #202020;
 }
-</script>
+.content {
+  width: 70%;
+  margin-left: 15%;
+}
+.author-image {
+  border-radius: 100%;
+  display: inline;
+  vertical-align: middle;
+  width: 20%;
+  max-width: 100px;
+  height: auto;
+}
+</style>
---
+++ src/pages/Author.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Author />
+  </div>
+</template>
+
+<script>
+import Author from '~/components/Author.vue'
+
+export default {
+  components: { Author }
+}
+</script>
